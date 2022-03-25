bbdddd81f033087659e061b8cb8a5c0bc32cea9b
Vaiishnaviie <94374230+Vaiishnaviie@users.noreply.github.com>
2022-03-25 21:54:12 +0530

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 21:18:58 2022
+
+@author: DELL
+"""
+
+from turtle import *
+color("red")
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
